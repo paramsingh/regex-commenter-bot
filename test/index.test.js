4375467a766393890587d058ee7c7dd39a4f6778
Suyash Garg <suyashgargsfam@gmail.com>
2020-05-11 23:29:52 +0200

    Back to javascript from TS

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,19 @@
-// You can import your modules
-// import index from '../src/index'
-
-import nock from 'nock'
+const nock = require('nock')
 // Requiring our app implementation
-import myProbotApp from '../src'
-import { Probot } from 'probot'
+const myProbotApp = require('..')
+const { Probot } = require('probot')
 // Requiring our fixtures
-import payload from './fixtures/issues.opened.json'
+const payload = require('./fixtures/issues.opened')
 const issueCreatedBody = { body: 'Thanks for opening this issue!' }
 const fs = require('fs')
 const path = require('path')
 
 describe('My Probot app', () => {
-  let probot: any
-  let mockCert: string
+  let probot
+  let mockCert
 
-  beforeAll((done: Function) => {
-    fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err: Error, cert: string) => {
+  beforeAll((done) => {
+    fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err, cert) => {
       if (err) return done(err)
       mockCert = cert
       done()
@@ -30,7 +27,7 @@ describe('My Probot app', () => {
     probot.load(myProbotApp)
   })
 
-  test('creates a comment when an issue is opened', async (done) => {
+  test('creates a comment when an issue is opened', async () => {
     // Test that we correctly return a test token
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
@@ -38,8 +35,8 @@ describe('My Probot app', () => {
 
     // Test that a comment is posted
     nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody))
+      .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
+        expect(body).toMatchObject(issueCreatedBody)
         return true
       })
       .reply(200)
@@ -56,9 +53,6 @@ describe('My Probot app', () => {
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
 
 // For more information about testing with Nock see:
 // https://github.com/nock/nock
