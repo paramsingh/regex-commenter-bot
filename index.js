c717ef8c0bc8507a4ae8587b2341d5d258fd740b
Param Singh <iliekcomputers@gmail.com>
2020-05-16 11:45:04 +0100

    Allow for a config file in .github to configure regex

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
-// https://glitch.com/edit/#!/bittersweet-hexagonal-message?path=index.js%3A49%3A0
-const regexes = ['.*pylint: disable*'] // TODO: put this in a yaml config file
-
-const matches = (pattern, text) => {
+// TODO: make it clear in the docs that the pattern is taken as a JS string
+const matchesPattern = (pattern, text) => {
   const regex = new RegExp(pattern);
   return !!text.match(regex)
 };
 
 module.exports = (app) => {
+  app.on(['pull_request.opened', 'pull_request.reopened', 'pull_request.synchronize'], async context => {
+    const owner = context.payload.repository.owner.login;
+    const repo = context.payload.repository.name;
+    const pullNumber = context.payload.pull_request.number;
+    const commitId = context.payload.pull_request.head.sha;
 
-    app.on(['pull_request.opened', 'pull_request.reopened', 'pull_request.synchronize'], async context => {
-        const owner = context.payload.repository.owner.login;
-        const repo = context.payload.repository.name;
-        const pullNumber = context.payload.pull_request.number;
-        const commitId = context.payload.pull_request.head.sha;
+    const config = await context.config('config.yaml');
 
-        const files = await context.github.pulls.listFiles({
-            owner,
-            repo,
-            number: pullNumber,
-            mediaType: {
-              format: "diff",
-            },
-        });
+    const files = await context.github.pulls.listFiles({
+        owner,
+        repo,
+        number: pullNumber,
+        mediaType: {
+          format: "diff",
+        },
+    });
 
-        // TODO: check that we haven't already commented
-        // TODO: Might not be fetching ALL the files because of pagination
-        files.data.forEach((file) => {
-          const patch = file.patch;
-          let position = 0;
-          patch.split("\n").forEach((line) => {
-            if (line.startsWith("+")) {
-              regexes.forEach((regex) => {
-                if (matches(regex, line)) {
-                  context.github.pulls.createComment({
-                    owner,
-                    repo,
-                    number: pullNumber,
-                    commit_id: commitId,
-                    position,
-                    path: file.filename,
-                    body: "this is a comment" // TODO: get this comment from config
-                  });
-                }
-              })
+    // TODO: check that we haven't already commented
+    // TODO: Might not be fetching ALL the files because of pagination
+    files.data.forEach((file) => {
+      const patch = file.patch;
+
+      // if it is an empty file, patch is undefined
+      if (!file.patch) {
+        return;
+      }
+
+      let position = 0;
+      patch.split("\n").forEach((line) => {
+        if (line.startsWith("+")) {
+          const addedLine = line.slice(1); // doing this to remove the + from the diff
+
+          // get each pattern from the config and try to match.
+          const matches = config.matches;
+          matches.forEach((match) => {
+            console.error(addedLine);
+            if (matchesPattern(match.regex, addedLine)) {
+              context.github.pulls.createComment({
+                owner,
+                repo,
+                number: pullNumber,
+                commit_id: commitId,
+                position,
+                path: file.filename,
+
+                // default to default comment if match doesn't have an associated comment
+                body: match.comment ? match.comment : config.defaults.comment
+              });
             }
-            position += 1;
           })
-        })
+        }
+        position += 1;
+      })
     })
+  })
 }
